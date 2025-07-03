10fc18b9b2587d3e7cdee1cc8de57ebd0249d121
Kamil Koziol <koziol.kamil@protonmail.com>
2025-07-03 20:11:17 +0200

    Respond with 200

--- internal/http/server.go
+++ internal/http/server.go
@@ -5,6 +5,10 @@ import (
 	"net"
 )
 
+func response(statusCode int, reason string) []byte {
+	return fmt.Appendf(nil, "HTTP/1.1 %d %s\r\n\r\n", statusCode, reason)
+}
+
 type Server struct{}
 
 func (s *Server) Run(hostport string) error {
@@ -12,10 +16,16 @@ func (s *Server) Run(hostport string) error {
 	if err != nil {
 		return fmt.Errorf("failed to bind to %s: %w", hostport, err)
 	}
+	defer l.Close()
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		return fmt.Errorf("error accepting connection: %w", err)
+	}
+
+	_, err = conn.Write(response(200, "OK"))
+	if err != nil {
+		return fmt.Errorf("unable to write: %w", err)
 	}
 
 	return nil
